52b88f7f32d6a95e2cc74b309bd6b2c4d2d704a4
anatoly <tolyan.yalta@yandex.ru>
2022-08-30 20:58:29 +0300

    Add Task_2

---
+++ Task_2/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе, 
+// которые являются делителями этого числа. 
+// (для получения цифр числа операцию приведения числа к строке не использовать)
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number < 10)
+{
+    Console.WriteLine("Цифра одна, не соответствует постановке задачи");
+}
+else
+{
+    int result = 0;
+    int temp = number;
+    while(temp > 0)
+    {
+        int digit = temp % 10;
+        temp = temp / 10;
+        if(digit != 0)
+        {
+            if(number % digit == 0)
+            {
+                result = result + digit;
+            }
+        }
+    }
+
+    Console.WriteLine("Результат = " + result);
+}
+
